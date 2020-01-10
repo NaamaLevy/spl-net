8357c7753ffe79f054864b3fc57d2bae57969a9a
Naama-Le <naamalevy25@gmail.com>
2020-01-10 18:44:42 +0200

    ConnectionsImpl

--- src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -2,25 +2,39 @@ package bgu.spl.net.srv;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
 
 public class ConnectionsImpl<T> implements Connections<T>{
 
     //fields
-    private ConcurrentHashMap<Integer, ConcurrentHashMap<String,Integer>> usersMap;
+    private ConcurrentHashMap<Integer, ConcurrentHashMap<String,Integer>> subscribersMap;
     private ConcurrentHashMap<String, ConcurrentHashMap<Integer,Integer>> topicsMap;
+    private ConcurrentHashMap<Integer, ConnectionHandler<T>> usersMap;
+    private  final Object lock;
 
     public ConnectionsImpl(){
+        subscribersMap = new ConcurrentHashMap<>();
+        topicsMap = new ConcurrentHashMap<>();
         usersMap = new ConcurrentHashMap<>();
-        usersMap = new ConcurrentHashMap<>();
+        lock = new Object();
     }
     @Override
     public boolean send(int connectionId, T msg) {
-        return false;
+        if (!usersMap.containsKey(connectionId))
+            return false;
+        synchronized (lock){
+            usersMap.get(connectionId).send(msg);
+            return true;
+        }
     }
 
     @Override
     public void send(String channel, T msg) {
+        ConcurrentHashMap topicSubsMap= topicsMap.get(channel);
+        if (topicSubsMap != null){
+           for()
 
+            }
     }
 
     @Override
