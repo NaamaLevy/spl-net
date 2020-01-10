780b64bdf1e1e6834dec93233b66be67aa26f844
Naama-Le <naamalevy25@gmail.com>
2020-01-10 19:13:49 +0200

    ConnectionsImpl

--- src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -4,25 +4,26 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 
-public class ConnectionsImpl<T> implements Connections<T>{
+public class ConnectionsImpl<T> implements Connections<T> {
 
     //fields
-    private ConcurrentHashMap<Integer, ConcurrentHashMap<String,Integer>> subscribersMap;
-    private ConcurrentHashMap<String, ConcurrentHashMap<Integer,Integer>> topicsMap;
+    private ConcurrentHashMap<Integer, ConcurrentHashMap<String, Integer>> subscribersMap;
+    private ConcurrentHashMap<String, ConcurrentHashMap<Integer, Integer>> topicsMap;
     private ConcurrentHashMap<Integer, ConnectionHandler<T>> usersMap;
-    private  final Object lock;
+    private final Object lock;
 
-    public ConnectionsImpl(){
+    public ConnectionsImpl() {
         subscribersMap = new ConcurrentHashMap<>();
         topicsMap = new ConcurrentHashMap<>();
         usersMap = new ConcurrentHashMap<>();
         lock = new Object();
     }
+
     @Override
     public boolean send(int connectionId, T msg) {
         if (!usersMap.containsKey(connectionId))
             return false;
-        synchronized (lock){
+        synchronized (lock) {
             usersMap.get(connectionId).send(msg);
             return true;
         }
@@ -30,12 +31,12 @@ public class ConnectionsImpl<T> implements Connections<T>{
 
     @Override
     public void send(String channel, T msg) {
-        ConcurrentHashMap topicSubsMap= topicsMap.get(channel);
-        if (topicSubsMap != null){
-           for( Object sub : topicSubsMap.values()){
-               usersMap.get((Integer)sub).send(msg);
-           }
+        ConcurrentHashMap topicSubsMap = topicsMap.get(channel);
+        if (topicSubsMap != null) {
+            for (Object sub : topicSubsMap.values()) {
+                usersMap.get((Integer) sub).send(msg);
             }
+        }
     }
 
     @Override
