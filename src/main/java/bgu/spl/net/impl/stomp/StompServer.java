7da9d21badc3eb3cea9a0c118fb00abd899c5cd8
hillell <hillell@post.bgu.ac.il>
2020-01-23 17:49:21 +0200

    final server

--- src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -7,21 +7,23 @@ import bgu.spl.net.srv.StompMessagingProtocolImpl;
 public class StompServer {
 
     public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
 
-//        if(args[1] =="tpc"){
-            Server.threadPerClient(9999,
+        if(args[1].equals("tpc")){
+            Server.threadPerClient(
+                    Integer.parseInt(args[0]),  //port
                     ()-> new StompMessagingProtocolImpl(),
                     STOMPMessageEncoderDecoder::new).serve();
-//        }
-//        if(args[1] == "reactor"){
-        //    Server.reactor(
-    /*                Runtime.getRuntime().availableProcessors(),
-               //     Integer.parseInt(args[0]), //port
-                    7777,
+        }
+        if(args[1].equals("reactor")){
+            Server.reactor(
+                   Runtime.getRuntime().availableProcessors(),
+                    Integer.parseInt(args[0]), //port
                     () ->  new StompMessagingProtocolImpl(),
-                    STOMPMessageEncoderDecoder::new).serve();*/
-//
-//        }
+                    STOMPMessageEncoderDecoder::new).serve();
+
+       }
 
     }
 
